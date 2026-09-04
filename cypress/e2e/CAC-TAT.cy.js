881bdfbfdc41b80424cef470437100a118a3a7ff
Patrick Sousa do Nascimento <p.nascimento@akrsistemas.com.br>
2026-09-04 14:02:37 -0300

    fixing the app

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,5 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  beforeEach(() => cy.visit('./'))
+  beforeEach(() => cy.visit('./src/index.html'))
 
   const longtext = Cypress._.repeat('Cypress é massa d+ ', 20)
   const data = {
